24eae11f7b3b6812144760c1059f3f2032233365
olethomas93 <norbye93@gmail.com>
2021-08-11 14:27:13 +0200

    black map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -32,12 +32,12 @@ export class MapComponent implements OnInit {
   @Output() coord$: EventEmitter<any> = new EventEmitter();
  public options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
         opacity: 0.7,
         maxZoom: 19,
         detectRetina: true,
         attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
       }),
     ],
     zoom: 12,
--- src/app/dialogs/tiles/tiles.component.ts
+++ src/app/dialogs/tiles/tiles.component.ts
@@ -3,6 +3,7 @@ import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { PubSub } from 'aws-amplify';
 import { title } from 'process';
+import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-tiles',
   templateUrl: './tiles.component.html',
@@ -12,12 +13,14 @@ export class TilesComponent {
   subscription: any;
 
   cardData =[
-    {title:"Wind",value:5,color:"green",icon:"air",unit:"m/s"},
+    {title:"Wind speed",value:5,color:"green",icon:"air",unit:"m/s"},
     {title:"Temperature",value:12,color:"green",icon:"thermostat",unit:"°C"},
-    {title:"air pressure",
+    {title:"Air pressure",
   value:1013,color:"green",icon:"storm",unit:"hPa"},
-    {title:"Sea Level",
-  value :2,color:"green",icon:"waves",unit:"m"}
+    {title:"Wind Direction",
+  value :2,color:"green",icon:"explore",unit:"°"},
+  {title:"Air Density",
+  value :2,color:"green",icon:"reorder",unit:"kg/m^3"}
   ]
   /** Based on the screen size, switch from standard to one column per row */
   cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -36,7 +39,12 @@ export class TilesComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver, public dialogRef: MatDialogRef<TilesComponent>) {
+  
+  }
+
+  ngOnInit(){
+
     this.subscription =  PubSub.subscribe('1.5.0/Sulesund/TagValues').subscribe({
       next: data =>this.updateData(data.value.TagData[0].Values),
       error: error => console.error(error)
@@ -47,15 +55,19 @@ export class TilesComponent {
 
 
   updateData(data:any){
-
+    console.log(data)
     this.cardData[0].value = Math.round(data.Vindhastighet) 
     this.cardData[1].value = Math.round(data.Temperatur)
     this.cardData[2].value = Math.round(data.Luft_Trykk)
+    this.cardData[3].value =Math.round(data.Vindretning)
+    this.cardData[4].value =Math.round(data.Luft_Tetthet)
 
 
   }
 
-  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
   ngOnDestroy():void{
 
     this.subscription.unsubscribe()
