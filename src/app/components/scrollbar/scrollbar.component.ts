e68ad050fdbbd258887198a3386a3eedbb6cea37
olethomas93 <norbye93@gmail.com>
2021-10-18 11:09:09 +0200

    slider

--- src/app/components/scrollbar/scrollbar.component.ts
+++ src/app/components/scrollbar/scrollbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { latLng, Map} from 'leaflet';
 @Component({
   selector: 'app-scrollbar',
@@ -6,13 +6,23 @@ import { latLng, Map} from 'leaflet';
   styleUrls: ['./scrollbar.component.scss']
 })
 export class ScrollbarComponent implements OnInit {
+  @ViewChild('container') el!:ElementRef
   @Input() docks!: Array<any>
   @Input() map!:Map
   scrollItems!: string[];
+
+  isDown!: boolean;
+  startX: any;
+  scrollLeft: any;
   
   constructor() { }
 
+
   ngOnInit(): void {
+
+this.isDown = false;
+this. startX;
+this.scrollLeft;
 
    
   }
@@ -21,6 +31,40 @@ export class ScrollbarComponent implements OnInit {
   flyToDock(location:any){
     this.map.flyTo(latLng(location[0],location[1],14))
 
+  }
+  onMouseDown(e:any){
+    
+    let slider =this.el.nativeElement
+    this.isDown = true;
+   slider.classList.add('active');
+    this.startX = e.pageX - slider.offsetLeft;
+    this.scrollLeft = slider.scrollLeft;
+
+  }
+  onMouseLeave(e:any){
+
+    let slider = this.el.nativeElement
+    this.isDown = false;
+    slider.classList.remove('active');
+  }
+
+  onMouseMove(e:any){
+   
+    let slider =this.el.nativeElement
+    
+    if(!this.isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - this.startX) * 1; //scroll-fast
+    slider.scrollLeft = this.scrollLeft - walk;
+    console.log(walk);
+
+
+  }
+  onMouseUp(e:any){
+    let slider =this.el.nativeElement
+    this.isDown = false;
+    slider.classList.remove('active');
   }
 
 }
