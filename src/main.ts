bd47e5d17dfadc74f6d28561c7260d2019f1e263
olethomas93 <norbye93@gmail.com>
2021-10-21 21:29:12 +0200

    jk

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import awsconfig from 'src/aws-exports';
-import Amplify from 'aws-amplify';
+import Amplify,{ PubSub } from 'aws-amplify';
 import { AWSIoTProvider } from '@aws-amplify/pubsub';
+
+
 // PubSub.configure(environment.awsmobile);
 // API.configure(environment.awsmobile)
 // Amplify.addPluggable(new AWSIoTProvider({
@@ -18,10 +20,10 @@ import { AWSIoTProvider } from '@aws-amplify/pubsub';
 
 
 Amplify.configure(awsconfig);
-// Amplify.addPluggable(new AWSIoTProvider({
-//   aws_pubsub_region: 'eu-central-1',
-//   aws_pubsub_endpoint: 'wss://a3vwh5519vcrt0-ats.iot.eu-central-1.amazonaws.com/mqtt',
-// }));
+Amplify.addPluggable(new AWSIoTProvider({
+  aws_pubsub_region: 'eu-central-1',
+  aws_pubsub_endpoint: 'wss://a3vwh5519vcrt0-ats.iot.eu-central-1.amazonaws.com/mqtt',
+}));
 
 // Apply plugin with configuration
 
