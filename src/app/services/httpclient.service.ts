86bde00ee1de5e4c092196d76c66cce6cd49f78b
olethomas93 <norbye93@gmail.com>
2021-12-14 20:23:31 +0100

    nk

--- src/app/services/httpclient.service.ts
+++ src/app/services/httpclient.service.ts
@@ -8,7 +8,10 @@ import { catchError, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class HttpclientService {
-
+  client_id="ole.theisen@brattvaag-electro.no:Fergekai"
+  scope="api"
+  client_secret="fergekaibrattvaagelectro"
+  grant_type ="client_credentials"
   private tokenUrl = "https://id.barentswatch.no/connect/token"
 
   constructor(
@@ -19,14 +22,24 @@ export class HttpclientService {
    }
 
    authenticate(): Observable<any>{
+    const headerDict = {
+      
+      
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+      'Access-Control-Allow-Credentials': "true"
+    }
+    
+    const requestOptions = {                                                                                                                                                                                 
+      headers: new HttpHeaders(headerDict), 
+    };
 
-    let header = new HttpHeaders()
-    header.set('Content-Type', 'application/x-www-form-urlencoded').set('Access-Control-Allow-Origin', '*');
 
-   return this.http.post(this.tokenUrl,{client_id:"ole.theisen@brattvaag-electro.no:Fergekai",
-    scope:"api",
-    client_secret:"fergekaibrattvaagelectro",
-    grant_type:"client_credentials"},{headers:header}).pipe(tap(
+ let body =`client_id=${this.client_id}&scope=api&client_secret=${this.client_secret}&grant_type=${this.grant_type}`
+   
+   return this.http.post<any>(this.tokenUrl,body,requestOptions).pipe(tap(
       error=>console.log(error)
      ))
 
