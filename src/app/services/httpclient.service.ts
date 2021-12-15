4f3c92ab1d5e365c77a90c09a333c17d9a8233dc
olethomas93 <norbye93@gmail.com>
2021-12-15 15:38:06 +0100

    cxcx

--- src/app/services/httpclient.service.ts
+++ src/app/services/httpclient.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders,HttpParams } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { catchError, tap } from 'rxjs/operators';
@@ -14,6 +14,11 @@ export class HttpclientService {
   grant_type ="client_credentials"
   private tokenUrl = "https://id.barentswatch.no/connect/token"
 
+  private bodyjson= { client_id:"ole.theisen@brattvaag-electro.no:Fergekai",
+  scope:"api",
+  client_secret:"fergekaibrattvaagelectro",
+  grant_type:"client_credentials"}
+
   constructor(
     private http: HttpClient
 
@@ -25,21 +30,29 @@ export class HttpclientService {
     const headerDict = {
       
       
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
-      'Access-Control-Allow-Credentials': "true"
+      'content-type': "application/x-www-form-urlencoded",
+      'cache-control': 'no-cache',
+      
     }
-    
+    const header = new HttpHeaders();
+
+    header.append("Content-Type","application/x-www-form-urlencoded")
+    header.append('Access-Control-Allow-Origin','*')
+   
     const requestOptions = {                                                                                                                                                                                 
       headers: new HttpHeaders(headerDict), 
+      withCredentials:true
+      
     };
+
+
+
+
 
 
  let body =`client_id=${this.client_id}&scope=api&client_secret=${this.client_secret}&grant_type=${this.grant_type}`
    
-   return this.http.post<any>(this.tokenUrl,body,requestOptions).pipe(tap(
+   return this.http.post<any>(this.tokenUrl,body,{headers:header,withCredentials:true}).pipe(tap(
       error=>console.log(error)
      ))
 
