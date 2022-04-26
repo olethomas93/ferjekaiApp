ebd5519c1d0747d026f3713a9fd734833deae880
ole.theisen <ole.theisen@brattvaag-electro.no>
2022-04-26 12:59:34 +0200

    grg

--- src/app/components/map-page/map-page.component.ts
+++ src/app/components/map-page/map-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewChildren } from '@angular/core';
 import { Map,polyline, circle,  markerClusterGroup, MarkerClusterGroup} from 'leaflet';
 import { HttpService } from 'src/app/services/http.service';
 import { PubSub } from 'aws-amplify';
@@ -9,7 +9,7 @@ import { interval, Subject, Subscriber, Subscription } from 'rxjs';
 import { timer } from 'rxjs';
 import { HttpclientService } from 'src/app/services/httpclient.service';
 import {WebsocketService} from '../../services/websocket.service'
-import 'leaflet.boatmarker'
+
 
 declare let L:any
 @Component({
@@ -32,7 +32,7 @@ export class MapPageComponent implements OnInit {
   ferrydocks :any;
   ferry:any
   ferryDocks:any[]=[]
-  ferrys:any[] =[{mmsi:257054910},{mmsi:257054390,id:"HADAROEY"},{mmsi:257054950,id:"SULOEY"},{mmsi:257090550,id:"SOLAVAGEN"},{mmsi:257090560,id:"FESTOEYA"}]
+  ferrys:any[] =[{mmsi:257054910},{mmsi:257054390,id:"HADAROEY"},{mmsi:257054950,id:"SULOEY"},{mmsi:257090550,id:"SOLAVAGEN"},{mmsi:257090560,id:"FESTOEYA"},{mmsi:257020700,id:"ULLENSVANG"},{mmsi:257062260,id:"ROVDEHORN"},{mmsi:257062510,id:"SKOPPHORN"},{mmsi:258932000,id:"FRAM"}]
   subscription2!: Subscription 
   private subscriptions = new Subscription()
 
@@ -45,13 +45,14 @@ export class MapPageComponent implements OnInit {
     private http: HttpService,
     private api: APIService,
     private httpclient:HttpclientService,
-    private socket :WebsocketService
+    private socket :WebsocketService,
+    private ElByClassName: ElementRef
     
     // private _formBuilder: FormBuilder,
   ) {
 
 
-
+this.createBoat()
  
 
 
@@ -77,9 +78,7 @@ export class MapPageComponent implements OnInit {
 
   ngOnInit(): void {
 
-
-
-    console.log("init map page")
+  
 
     this.markerClusterGroup = markerClusterGroup({removeOutsideVisibleBounds: true});
    
@@ -104,8 +103,7 @@ export class MapPageComponent implements OnInit {
     this.boatSubscrition.add(this.socket.getEvent().subscribe((event)=>{
 
      this.onFerryLocation(event)
- 
-
+   
    }))
 
       
@@ -128,8 +126,9 @@ export class MapPageComponent implements OnInit {
   
       if(location.type =="Position"){
       
-      ferry.layer = L.boatMarker([location.latitude,location.longitude],{color:"red",fillColor:"blue",stroke:true,idleCircle: true})
-  
+      ferry.layer = L.boatMarker([location.latitude,location.longitude],{class:'boats',color:"red",fillColor:"blue",stroke:true,idleCircle: true})
+
+   
       ferry.layer.setHeading(location.trueHeading);
       ferry.layer.setSpeed(location.speedOverGround)
       
@@ -204,113 +203,7 @@ this.api.ListDocks().then((data:any)=>{
 
   this.ferryDocks.push(ferry)
 
-  // this.api.GetDockData(this.ferrydocks[i].id).then((data:any)=>{
-    
-  //   if(data){
-  //   let res =data.alarms
-  //     let temp = false;
-  //   for( let i in res){
-  //     let status = (res[i].value.toLowerCase() === "true")
-      
-  //     if (status){
-
-  //       temp = true;
-  //     }
-
-
-  //   }
-  //     if (temp){
   
-  //     ferry.getElement()?.classList.add("pulse")
-  //     ferry.setStyle({color:"red",className:''})
-      
-  //   }else{
-  //     ferry.getElement()?.classList.remove("pulse")
-  //     ferry.setStyle({color:"green"})
-      
-      
-  
-  //   }
-
-    
-  
-      
-  
-    
-  //   }
-  // })
-
-  // this.api.OnUpdateByIdListener(this.ferrydocks[i].id).subscribe((data:any)=>{
-
-  //   let alarms =data.value.data.onUpdateById.alarms
-
-  //   let temp = false;
-  //   for(let i in alarms){
-
-  //     let status = (alarms[i].value.toLowerCase() === "true")
-      
-  //     if (status){
-
-  //       temp = true;
-  //     }
-  //   }
-
-     
-  //   if (temp){
-  
-  //     ferry.getElement()?.classList.add("pulse")
-  //     ferry.setStyle({color:"red",className:''})
-      
-  //   }else{
-  //     ferry.getElement()?.classList.remove("pulse")
-  //     ferry.setStyle({color:"green"})
-      
-      
-  
-  //   }
-
-   
-  
-
-  // })
-
-// this.subscription=  PubSub.subscribe(`fergekai/${this.ferrydocks[i].id}`,{}).subscribe((data)=>{
-  
-//     let res = data.value[this.ferrydocks[i].id]
-//     let dataName = Object.keys(res)[0];
-    
-//     if(dataName = "alarms"){
-//       let temp = false;
-//       for(let i in res.alarms){
-        
-//         let status = (res.alarms[i].value.toLowerCase() === "true")
-        
-//         if (status){
-  
-//           temp = true;
-//         }
-//       }
-  
-       
-//       if (temp){
-    
-//         ferry.getElement()?.classList.add("pulse")
-//         ferry.setStyle({color:"red",className:''})
-        
-//       }else{
-//         ferry.getElement()?.classList.remove("pulse")
-//         ferry.setStyle({color:"green"})
-        
-        
-    
-//       }
-
-
-//     }
-  
-  
-//    })
-
 
 const source =timer(5000);
   
@@ -522,7 +415,190 @@ this.openStatusDialog()
   receiveZoom(zoom: number) {
     this.zoom = zoom;
   }
+createBoat(){
 
+  /* BOAT ICON */
+L.BoatIcon = L.Icon.extend({
+   img:new Image(),
+
+	// OPTIONS
+	options: {
+		iconSize: new L.Point(20, 20),
+		className: "leaflet-boat-icon",
+		course: 0,
+		speed: 0,
+		color: "#8ED6FF",
+		labelAnchor: [23, 0],
+		wind: false,
+		windDirection: 0,
+		windSpeed: 0,
+		idleCircle: false,
+    x_fac: 0.10,
+	  y_fac: 0.10,
+	},
+
+	// PROPERTIES
+	x: 66,
+	y: 85,
+
+	ctx: null,
+	lastHeading: 0,
+	lastWindDirection: 0,
+
+	// CREATE ICON
+	// setup the icon and start drawing
+	createIcon: function () {
+		var e = document.createElement("canvas");
+		this._setIconStyles(e, "icon");
+		var s = this.options.iconSize;
+
+		e.width = s.x;
+		e.height = s.y;
+		this.lastHeading = 0;   // reset in case the marker is removed and added again
+
+		this.ctx = e.getContext("2d");
+		this.draw(this.ctx, s.x, s.y);
+
+		return e;
+	},
+
+	// DRAW
+	// renders the boat icon onto the canvas element
+	draw: function(ctx:any, w:any, h:any) {
+		if(!ctx) return;
+		var x = this.x;
+		var y = this.y;
+
+		var x_fac = this.options.x_fac;
+		var y_fac = this.options.y_fac;
+
+		ctx.clearRect(0, 0, w, h);
+
+		ctx.translate(w/2, h/2);
+		ctx.rotate(this.options.course*Math.PI/180);
+		ctx.translate(-w/2, -h/2);
+
+		//ctx.fillRect(0,0,w,h);
+
+		ctx.beginPath();
+
+		// draw idle boat shape
+		if(this.options.idleCircle === true && this.options.speed === 0) {
+			ctx.arc(10, 10, 5, 0, 2 * Math.PI);
+		}
+		// draw boat shape in motion
+		else {
+			// ctx.moveTo(x, y);
+			// ctx.bezierCurveTo(x, y+(80*y_fac), x+(100*x_fac), y+(80*y_fac), x+(100*x_fac), y);
+			// ctx.quadraticCurveTo(x+(100*x_fac), y-(100*y_fac), x+(50*x_fac), y-(200*y_fac));
+			// ctx.quadraticCurveTo(x, y-(100*y_fac), x, y);
+
+      var img =new Image();
+      img.onload = function(){
+        ctx.drawImage(img,0,0)
+      }
+      img.src = 'assets/boat.svg'
+		}
+
+		ctx.fillStyle = this.options.color;
+		ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
+
+		// draw wind
+		if(this.options.wind == true) {
+
+			ctx.translate(w/2, h/2);
+			ctx.rotate(this.options.windDirection*Math.PI/180);
+			ctx.translate(-w/2, -h/2);
+
+			ctx.beginPath();
+			ctx.moveTo(w/2, y-45);
+			ctx.lineTo(w/2, y-70);
+
+			var center = w/2;
+
+			var spd = 5 * Math.round(this.options.windSpeed / 5);
+			var tenLines = Math.floor(spd / 10);
+			var fiveLine = ((spd % 10) > 0);
+
+			var carriage = 70;
+			for(var i = 0; i < tenLines; i++) {
+				ctx.moveTo(center, y - carriage);
+				ctx.lineTo(center + 8, y - carriage - 8);
+				carriage -= 5;
+			}
+
+			if(fiveLine) {
+				if(tenLines == 0) carriage -= 5;
+				ctx.moveTo(center, y - carriage);
+				ctx.lineTo(center + 5, y - carriage - 5);
+			}
+
+      		ctx.stroke();
+		}
+	},
+
+	setHeadingWind: function(heading:any, windSpeed:any, windDirection:any) {
+		this.options.wind = true;
+
+		this.options.course = (heading % 360) - this.lastHeading;
+		this.lastHeading = heading % 360;
+
+		this.options.windDirection = (windDirection % 360) - (heading % 360);
+		this.lastHeading += this.options.windDirection;
+
+		this.options.windSpeed = windSpeed;
+
+		var s = this.options.iconSize;
+		this.draw(this.ctx, s.x, s.y);
+	},
+
+	// SET HEADING
+	// sets the boat heading and
+	// update the boat icon accordingly
+	setHeading: function(heading:any) {
+		this.options.course = (heading % 360) - this.lastHeading;
+		this.lastHeading = heading % 360;
+
+		var s = this.options.iconSize;
+		this.draw(this.ctx, s.x, s.y);
+	},
+
+	// SET SPEED
+	// sets the boat speed value and
+	// update the boat icon accordingly
+	setSpeed: function(speed:any) {
+		this.options.speed = speed;
+
+		var s = this.options.iconSize;
+		this.draw(this.ctx, s.x, s.y);
+	}
+});
+
+L.BoatMarker = L.Marker.extend({
+  	setHeadingWind: function(heading:any, windSpeed:any, windDirection:any) {
+  		this.options.icon.setHeadingWind(heading, windSpeed, windDirection);
+  	},
+
+  	setHeading: function(heading:any) {
+  		this.options.icon.setHeading(heading);
+  	},
+
+	setSpeed: function(speed:any) {
+		this.options.icon.setSpeed(speed);
+	}
+});
+
+L.boatMarker = function(pos:any, options:any) {
+
+	var c = ("color" in options) ? options.color : "#f1c40f";
+	var i = ("idleCircle" in options) ? options.idleCircle : false;
+	options.icon = new L.BoatIcon({ color: c, idleCircle: i});
+
+    return new L.BoatMarker(pos, options);
+};
+}
   
 }
 
